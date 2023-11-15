fe583c76767212e79995647f8d4e110f766aa38c
Josip Potnar <j.potnar@gmail.com>
2023-11-15 15:23:06 +0000

    Register admin view, run migrations

--- board/admin.py
+++ board/admin.py
@@ -1,23 +1,31 @@
 from django.contrib import admin
-from .models import Post
+from .models import Category, Task, Profile, Comment
 from django_summernote.admin import SummernoteModelAdmin
 
 
-@admin.register(Post)
-class PostAdmin(SummernoteModelAdmin):
-    summernote_fields = ('content')
-
-class PostAdmin():
-
-    list_display = ('title', 'slug', 'status', 'created_on', 'status')
+@admin.register(Task)
+class TaskAdmin(SummernoteModelAdmin):
+    list_display = ('title', 'status', 'created_date', 'status')
     search_fields = ['title', 'content']
-    prepopulated_fields = {'slug': ('title',)}
-    list_filter = ('status', 'created_on')
+    prepopulated_fields = {'id': ('title',)}
+    list_filter = ('status', 'created_date')
+    summernote_fields = ('content',)
 
 
 @admin.register(Comment)
-class CommentAdmin(SummernoteModelAdmin):
+class CommentAdmin(admin.ModelAdmin):
+    list_display = ('author', 'message', 'post', 'created_date')
+    list_filter = ('author', 'created_date')
+    search_fields = ['author', 'message', 'post']
 
-    list_display = ('name', 'body', 'post', 'created_on')
-    list_filter = ('created_on')
-    search_fields = ['name', 'email', 'body']
+@admin.register(Category)
+class CategoryAdmin(admin.ModelAdmin):
+    pass
+
+
+@admin.register(Profile)
+class ProfileAdmin(admin.ModelAdmin):
+    list_display = ('name', 'surname', 'location', 'city')
+    list_filter = ('location', 'city')
+    search_fields = ['name', 'surname', 'location', 'city', 'country']
+
--- board/models.py
+++ board/models.py
@@ -21,8 +21,8 @@ CATEGORIES = (
 # Create your models here.
 
 class Category (models.Model):
-    name = models.CharField(choices=CATEGORIES, null=False, blank=False)
-    image = models.CloudinaryField('image', default='placeholder')
+    name = models.CharField(choices=CATEGORIES, null=False, blank=False, max_length=100)
+    image = CloudinaryField('image', default='placeholder')
 
     def __str__(self):
         return self.name
@@ -32,41 +32,60 @@ class Task (models.Model):
     title = models.CharField(max_length=50, unique=True)
     owner = models.ForeignKey(User, on_delete=models.CASCADE, related_name="tasker")
     description = models.TextField
-    status = models.CharField(choices=STATUS, default="Draft")
+    status = models.CharField(choices=STATUS, default="Draft", max_length=100)
     helper = models.ForeignKey(User, on_delete=models.CASCADE, related_name="helper")
     created_date = models.DateTimeField(auto_now_add=True)
+    updated_date = models.DateTimeField(auto_now_add=True)
     category = models.ForeignKey(Category, on_delete=models.CASCADE, related_name="sort")
+    final_date = models.DateField(blank=True)
 
     class Meta:
-        ordering = ['created_on']
+        ordering = ['created_date']
 
     def __str__(self):
         return self.title
 
+    @property
+    def countdown(self):
+        today = date.today()
+        days_time_left = self.final_date - today
+        days_left = str(days_time_left).split(',', 1)[0]
+        return days_left
+
 
 class Comment(models.Model):
-    post = models.ForeignKey(Post, on_delete=models.CASCADE, related_name="comments")
-    author = models.ForeignKey()
+    post = models.ForeignKey(Task, on_delete=models.CASCADE, related_name="comments")
+    author = models.ForeignKey(User, on_delete=models.CASCADE, related_name="commenter")
     message = models.TextField()
-    created_on = models.DateTimeField(auto_now_add=True)
+    created_date = models.DateTimeField(auto_now_add=True)
 
 
     class Meta:
-        ordering = ['created_on']
+        ordering = ['created_date']
 
 
     def __str__(self):
-        return f'{self.name}: "{self.message}"'
+        return f'{self.author.user.profile.name}: "{self.message}"'
 
 
 class Profile(models.Model):
-    address = CharField(max_length=100)
-    location = CharField(max_length=60)
-    city = CharField(max_length=50)
+    address = models.CharField(max_length=100)
+    location = models.CharField(max_length=60)
+    city = models.CharField(max_length=50)
     country = CountryField()
-    name = CharField(max_length=50)
-    surname = CharField(max_length=50)
+    name = models.CharField(max_length=50)
+    surname = models.CharField(max_length=50)
     person = models.OneToOneField(User, on_delete=models.CASCADE)
+    avatar = CloudinaryField('image', default='placeholder-avatar')
 
     def __str__(self):
         return self.name + ' ' + self.surname
+
+
+# class Review(models.Model):
+#     user = models.ForeignKey(User, on_delete=models.CASCADE)
+#     karma = models.ManyToManyField(User, related_name='karma_points', blank=True)
+#     post = models.ForeignKey(Task, on_delete=models.CASCADE, related_name="task")
+
+#     def karma_points(self):
+#         return self.karma.count() / self.karma.len
