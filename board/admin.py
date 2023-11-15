b1dffff74f2a4a87d7a9dcbbb8ddebfc0ba7fa59
Josip Potnar <j.potnar@gmail.com>
2023-11-15 12:30:40 +0000

    Create and update models

--- board/admin.py
+++ board/admin.py
@@ -1,8 +1,23 @@
 from django.contrib import admin
 from .models import Post
+from django_summernote.admin import SummernoteModelAdmin
 
 
-admin.site.register(Post)
+@admin.register(Post)
+class PostAdmin(SummernoteModelAdmin):
+    summernote_fields = ('content')
+
+class PostAdmin():
+
+    list_display = ('title', 'slug', 'status', 'created_on', 'status')
+    search_fields = ['title', 'content']
+    prepopulated_fields = {'slug': ('title',)}
+    list_filter = ('status', 'created_on')
 
 
-# Register your models here.
+@admin.register(Comment)
+class CommentAdmin(SummernoteModelAdmin):
+
+    list_display = ('name', 'body', 'post', 'created_on')
+    list_filter = ('created_on')
+    search_fields = ['name', 'email', 'body']
--- board/models.py
+++ board/models.py
@@ -1,18 +1,41 @@
 from django.db import models
 from django.contrib.auth.models import User
+from cloudinary.models import CloudinaryField
+from django_countries.fields import CountryField
 
 
-STATUS = ((0, "Draft") (1, "Published"), (2, "Ongoing"), (3, "Archived"))
+STATUS = (
+    ("Draft", "Draft"),
+    ("Published", "Published"),
+    ("Ongoing", "Ongoing"),#
+    ("Archived", "Archived"))
+CATEGORIES = (
+    ("Helping the elderly", "Helping the elderly"),
+    ("Helping the youth", "Helping the youth"),
+    ("Helping the people with disability", "Helping the people with disability"),
+    ("Grocery run", "Grocery run"),
+    ("DIY, minor home improvements", "DIY, minor home improvements"),
+    ("Urgent", "Urgent"),
+    ("Other", "Other"),
+    )
 # Create your models here.
-class Post (models.Model):
-    title = models.CharField(max_length=30, unique=True)
-    slug = models.SlugField(max_length=30, unique=True)
-    author = models.ForeignKey(User, on_delete=models.CASCADE, related_name="tasker")
-    content = models.TextField
-    status = models.IntegerField(choices=STATUS, default=0)
-    featured_image = CloudinaryField('image', default='placeholder')
+
+class Category (models.Model):
+    name = models.CharField(choices=CATEGORIES, null=False, blank=False)
+    image = models.CloudinaryField('image', default='placeholder')
+
+    def __str__(self):
+        return self.name
+
+
+class Task (models.Model):
+    title = models.CharField(max_length=50, unique=True)
+    owner = models.ForeignKey(User, on_delete=models.CASCADE, related_name="tasker")
+    description = models.TextField
+    status = models.CharField(choices=STATUS, default="Draft")
     helper = models.ForeignKey(User, on_delete=models.CASCADE, related_name="helper")
-    created_on = models.DateTimeField(auto_now_add=True)
+    created_date = models.DateTimeField(auto_now_add=True)
+    category = models.ForeignKey(Category, on_delete=models.CASCADE, related_name="sort")
 
     class Meta:
         ordering = ['created_on']
@@ -23,9 +46,8 @@ class Post (models.Model):
 
 class Comment(models.Model):
     post = models.ForeignKey(Post, on_delete=models.CASCADE, related_name="comments")
-    name = models.CharField(max_length=80)
-    email = models.EmailField()
-    body = models.TextField()
+    author = models.ForeignKey()
+    message = models.TextField()
     created_on = models.DateTimeField(auto_now_add=True)
 
 
@@ -34,4 +56,17 @@ class Comment(models.Model):
 
 
     def __str__(self):
-        return f"Comment {self.body} by {self.name}"
+        return f'{self.name}: "{self.message}"'
+
+
+class Profile(models.Model):
+    address = CharField(max_length=100)
+    location = CharField(max_length=60)
+    city = CharField(max_length=50)
+    country = CountryField()
+    name = CharField(max_length=50)
+    surname = CharField(max_length=50)
+    person = models.OneToOneField(User, on_delete=models.CASCADE)
+
+    def __str__(self):
+        return self.name + ' ' + self.surname
