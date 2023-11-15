502008bca397f6f15fe5dae640226da21fb5a411
Josip Potnar <j.potnar@gmail.com>
2023-11-15 16:12:27 +0000

    Register home view in urls

--- board/views.py
+++ board/views.py
@@ -1,3 +1,8 @@
-from django.shortcuts import render
+from django.shortcuts import render, HttpResponse
+# from .models import Task
 
-# Create your views here.
+
+
+
+def home(request):
+    return HttpResponse("Hello World!")
--- communityhelper/urls.py
+++ communityhelper/urls.py
@@ -17,6 +17,7 @@ from django.contrib import admin
 from django.urls import path, include
 
 urlpatterns = [
+    path('', include('board.urls'), name='board_urls'),
     path('admin/', admin.site.urls),
     path('summernote/', include('django_summernote.urls'))
 ]
