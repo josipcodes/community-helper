d87385d963b06f86dd16bfd381430ca9f16c3268
Josip Potnar <j.potnar@gmail.com>
2023-11-23 14:33:13 +0000

    Add pagination to get_task_list in views

--- board/views.py
+++ board/views.py
@@ -1,4 +1,5 @@
 from django.shortcuts import render, redirect
+from django.core.paginator import Paginator
 from .models import Task, User
 from .forms import TaskForm
 
@@ -7,7 +8,7 @@ from .forms import TaskForm
 # renders index.html
 def home(request):
     # below logic was taken (but custoomized) from:
-    # https://stackoverflow.com/questions/53243835/django-how-to-display-number-of-items
+    # https://www.learningaboutelectronics.com/Articles/How-to-count-all-objects-of-a-database-table-in-Django.php
     published_tasks = Task.objects.filter(status="Published")
     archived_tasks = Task.objects.filter(status="Archived")
     total_published = published_tasks.count()
@@ -39,19 +40,20 @@ def new_task(request):
 
 
 def get_task_list(request):
-    tasks = Task.objects.all()
+    task_list = Task.objects.all()
+    # paginator logic copied from:
+    # https://docs.djangoproject.com/en/4.2/topics/pagination/
+    paginator = Paginator(task_list, 6)
+    page_number = request.GET.get("page")
+    page_obj = paginator.get_page(page_number)
     context = {
-        'tasks': tasks
+        'task_list': task_list,
+        "page_obj": page_obj
     }
     return render(request, "list_tasks.html", context)
 
 
-# def total_tasks(request):
-#     count_of_tasks = Task.objects.all().count()
-#     context = {
-#         'count_of_tasks': count_of_tasks
-#     }
-#     return render(request, "index.html", context)
+
 
 
 
