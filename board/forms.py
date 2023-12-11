dd530a49354920aaf32cf2b460c6fe9e482f12c1
Josip Potnar <j.potnar@gmail.com>
2023-12-11 15:14:45 +0000

    Add deadline to task form, update task display

--- board/forms.py
+++ board/forms.py
@@ -1,3 +1,4 @@
+from django import forms
 from django.forms import ModelForm
 from .models import Task, Comment
 
@@ -7,11 +8,28 @@ class TaskForm(ModelForm):
         model = Task
         # fields used
         fields = [
-            "title",
-            "description",
-            "category",
-            # "final_date",
+            'title',
+            'description',
+            'category',
+            'final_date',
         ]
+
+        labels = {
+            'title': 'Task Title',
+            'description': 'Description of your task',
+            'category': 'Category',
+            'final_date': 'Deadline',
+        }
+
+        widgets = {
+            'title': forms.TextInput(attrs={'class':'form-control', 'placeholder':'Enter the Title'}),
+            'description': forms.Textarea(attrs={'class':'form-control', 'placeholder':'Enter the Description'}),
+            'category': forms.Select(attrs={'class':'form-control'}),
+            # datepicker created with the help of below:
+            # https://stackoverflow.com/questions/
+            # 3367091/whats-the-cleanest-simplest-to-get-running-datepicker-in-django
+            'final_date': forms.widgets.DateInput(attrs={'class':'form-control', 'type': 'date'})
+        }
 
 
 # CommentForm class
@@ -20,6 +38,6 @@ class CommentForm(ModelForm):
         model = Comment
         # fields used
         fields = [
-            "message",
+            'message',
         ]
 
