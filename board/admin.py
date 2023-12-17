262c980eb915ace7521714ffdd6c0115db01a418
Josip Potnar <j.potnar@gmail.com>
2023-12-17 13:52:37 +0000

    Add docstrings to admin

--- board/admin.py
+++ board/admin.py
@@ -5,6 +5,10 @@ from django_summernote.admin import SummernoteModelAdmin
 
 @admin.register(Task)
 class TaskAdmin(SummernoteModelAdmin):
+    '''
+    Register TaskAdmin.
+    TaskAdmin class organises display, search, filter and summernote field
+    '''
     list_display = ('title', 'owner', 'created_date', 'status')
     search_fields = ['title', 'description']
     list_filter = ('status', 'created_date')
@@ -13,17 +17,28 @@ class TaskAdmin(SummernoteModelAdmin):
 
 @admin.register(Comment)
 class CommentAdmin(admin.ModelAdmin):
+    '''
+    Register CommentAdmin.
+    CommentAdmin class organises display, search and filter
+    '''
     list_display = ('author', 'message', 'post', 'created_date')
     list_filter = ('author', 'created_date')
     search_fields = ['author', 'message', 'post']
 
 @admin.register(Category)
 class CategoryAdmin(admin.ModelAdmin):
+    '''
+    Registerting CategoryAdmin.
+    '''
     pass
 
 
 @admin.register(Profile)
 class ProfileAdmin(admin.ModelAdmin):
+    '''
+    Register ProfileAdmin.
+    ProfileAdmin class organises display, search and filter
+    '''
     list_display = ('person', 'name', 'surname', 'location', 'city')
     list_filter = ('location', 'city')
     search_fields = ['person', 'name', 'surname', 'location', 'city', 'country']
